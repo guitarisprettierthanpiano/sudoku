ca9ebf4faf60aff13283afec7e003fd284629d09
shfabrizio <shfabrizio@gmail.com>
2022-01-27 20:07:02 -0500

    added a working reset button

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,16 @@ import Main from './components/main'
 
 const App: React.FC = () => {
 
+    //clicking the reset button will reset the cell values to 0 and make them transparent again
+    const ResetPuzzle = () => {
+        const puzzle_array: NodeListOf<HTMLElement> = document.querySelectorAll('.hoverable')
+
+        for (let i = 0; i < puzzle_array.length; i++){
+            puzzle_array[i].style.color = 'rgba(0,0,0,0)';
+            puzzle_array[i].innerHTML = '0';
+        }
+    }
+
 
     return(
         <>
@@ -11,7 +21,9 @@ const App: React.FC = () => {
         
         <Main/>
 
-        <div className='right-side'></div>
+        <div className='right-side'>
+            <button onClick = {() => ResetPuzzle()}>reset puzzle</button>
+        </div>
 
         </> 
     )
